190250922ae0ffa4ec07d62ab0a500c6d69fc60b
immunochomik <tomaszswider4@gmail.com>
2016-10-14 06:01:04 +0100

    rename app to application in application.py

--- application.py
+++ application.py
@@ -1,43 +1,42 @@
 from flask import Flask, url_for, render_template
 import json
-app = Flask(__name__)
-app.config['DEBUG'] = True
+application = Flask(__name__)
+application.config['DEBUG'] = True
 
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
+# Note: We don't need to call run() since our applicationlication is embedded within
+# the application Engine WSGI applicationlication server.
 
 
-@app.route('/')
+@application.route('/')
 def home():
     """Return a friendly HTTP greeting."""
     return render_template('home.html');
 
 
-@app.route('/test')
+@application.route('/test')
 def test():
     """Return a friendly HTTP greeting."""
     return 'Test';
 
 
 
-@app.route('/image', methods=['GET', 'POST', 'PUT'])
+@application.route('/image', methods=['GET', 'POST', 'PUT'])
 def get_edges():
     """Post inage and return edges base foe maze"""
-    import cv2
     img = cv2.imread('gate-6.jpg',0)
     edges = cv2.Canny(img,0,200)
     ret = []
     for row in edges:
         ret_row = []
         for point in row:
-            ret_row.append('0' if str(point) == '0' else '1')
-        ret.append(ret_row)
+            ret_row.applicationend('0' if str(point) == '0' else '1')
+        ret.applicationend(ret_row)
     resp = flask.Response()
-    resp.headers['Content-Type'] = 'application/json'
+    resp.headers['Content-Type'] = 'applicationlication/json'
     return json.dumps(ret)
 
 
-@app.errorhandler(404)
+@application.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
@@ -45,7 +44,7 @@ def page_not_found(e):
 
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
+    # removed before deploying a production application.
     application.debug = True
     application.run()
 
